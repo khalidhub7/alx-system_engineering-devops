037453af38c7a177faed434d065a172a9fae9a55
khalidhub7 <louguikhalid7@gmail.com>
2024-01-29 05:58:28 -0800

    up

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,6 +1,6 @@
 /*
  * File: 102-zombie.c
- * Auth: Brennan D Baraban
+ * Auth: Brennan D Baraban && khalid log hhh
  */
 
 #include <stdio.h>
@@ -16,11 +16,11 @@
  */
 int infinite_while(void)
 {
-        while (1)
-        {
-                sleep(1);
-        }
-        return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -30,23 +30,22 @@ int infinite_while(void)
  */
 int main(void)
 {
-        pid_t pid;
-        char count = 0;
+	pid_t pid;
+	char count = 0;
 
-        while (count < 5)
-        {
-                pid = fork();
-                if (pid > 0)
-                {
-                        printf("Zombie process created, PID: %d\n", pid);
-                        sleep(1);
-                        count++;
-                }
-                else
-                        exit(0);
-        }
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0)
+		}
+	infinite_while();
 
-        infinite_while();
-
-        return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
