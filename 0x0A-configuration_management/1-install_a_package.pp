b098496869605bb93a2ed5165b610354cd941a0f
khalidhub7 <louguikhalid7@gmail.com>
2024-04-19 14:44:48 -0700

    ssh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,18 @@
 # install package
+$flask_version = '2.1.0'
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure     => $flask_version,
+  provider   => 'pip3',
   install_options => ['--upgrade'],  # Upgrade if already installed
-  uninstall_options => ['--force'], # Force uninstall if present but not desired version
+  uninstall_options => ['--force'],  # Force uninstall if present but not desired version
+  install_only => false,             # Install even if already present at the specified version
   allow_virtual => false,            # Do not use virtual packages
-  notify => Exec['restart_flask'],   # Notify a service to restart if Flask is updated
+  notify     => Exec['restart_flask'], # Notify a service to restart if Flask is updated
 }
 
 exec { 'restart_flask':
-  command => '/path/to/restart_flask_service', # Command to restart Flask service
-  refreshonly => true,                         # Only run this command if notified by the package resource
-  subscribe => Package['flask'],                # Subscribe to changes in the Flask package
+  command     => '/path/to/restart_flask_service', # Command to restart Flask service
+  refreshonly => true,                             # Only run this command if notified by the package resource
+  subscribe   => Package['flask'],                  # Subscribe to changes in the Flask package
 }
