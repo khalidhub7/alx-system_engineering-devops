4a7c5429b1a553f60253945f2f3c3d34049424fe
khalidhub7 <louguikhalid7@gmail.com>
2024-04-19 14:32:30 -0700

    ssh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Install package
+$flask_installed = $facts['os']['flask_installed']
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  ignore   => 'failure',
+  unless   => $flask_installed,
 }
