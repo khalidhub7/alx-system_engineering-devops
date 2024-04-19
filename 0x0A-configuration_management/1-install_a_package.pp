dacd1b4d5fb90ca1794abf521ea6f1e15141ef02
khalidhub7 <louguikhalid7@gmail.com>
2024-04-19 14:49:59 -0700

    ssh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,6 @@
 # install package
-$flask_version = '2.1.0'
-
 package { 'flask':
-  ensure          => $flask_version,
-  provider        => 'pip3',
-  install_options => ['--upgrade'],
-  uninstall_options => ['--force'],
-  install_only    => false,
-  allow_virtual   => false,
-  notify          => Exec['restart_flask'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
-exec { 'restart_flask':
-  command     => '/path/to/restart_flask_service',
-  refreshonly => true,
-  subscribe   => Package['flask'],
-}
+puppet apply install_flask.pp
