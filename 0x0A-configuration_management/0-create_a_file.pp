d8810d013258b2c28bc4990e1a33703613aef97a
khalidhub7 <louguikhalid7@gmail.com>
2024-05-03 19:34:09 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Using Puppet, create a file with some Requirements
-File {'/tmp/school':
+#  create a file using Puppet
+file {'/tmp/school':
     ensure  => file,
+    content => 'I love Puppet',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    content => 'I love Puppet',
 }
