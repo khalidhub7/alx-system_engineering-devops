2ff5ed32f690b6c1432a6c6ad91411fee9318efb
khalidhub7 <louguikhalid7@gmail.com>
2024-05-04 21:29:46 -0700

    config ssh using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-exec { "ssh configuration":
-	command => "echo -e 'HOST *\n\tPasswordAuthentication no\nHOST ubuntu\n\tHostName 100.27.2.194\n\tPort 22\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school' > ~/.ssh/school",
-	path    => ['/bin', '/usr/bin'],
+exec { 'ssh_configuration':
+    command => "echo -e 'HOST *\n\tPasswordAuthentication no\nHOST ubuntu\n\tHostName 100.27.2.194\n\tPort 22\n\tUser ubuntu\n\tIdentityFile ~/.ssh/school' > /etc/ssh/ssh_config",
+    path    => ['/bin', '/usr/bin'],
 }
