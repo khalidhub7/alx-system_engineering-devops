090d9c852eaf87e3d85c31023d8c65de69e5fc54
khalidhub7 <louguikhalid7@gmail.com>
2024-05-23 09:49:21 -0700

    up

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+"""Return info about todo list of an employee by ID
+but in CSV format"""
 
-import csv
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    employee_ID = int(argv[1])
-    person = requests.get("\
-https://jsonplaceholder.typicode.com/users/{}".format(employee_ID)).json()
-    todo_list = requests.get("\
-https://jsonplaceholder.typicode.com/users/{}/todos\
-".format(employee_ID)).json()
+if __name__ == '__main__':
+    id = int(argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    data = requests.get(url).json()
+    todo_list = requests.get('{}/todos'.format(url)).json()
 
-    with open('{}.csv'.format(employee_ID), 'w') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
-        for i in todo_list:
-            writer.writerow([str(employee_ID), person["username"],
-                             str(i["completed"]),
-                             i["title"]])
+    file = '{}.csv'.format(id)
+    csv_list = []
+    for i in todo_list:
+        csv_list.append("'{}','{}','{}','{}'".format(
+            i.get('userId'), data['name'], i.get('completed'), i.get('title')))
+    csv_content = ""
+    for j in csv_list:
+        csv_content += j
+        if j != csv_list[-1]:
+            csv_content += '\n'
+    with open(file, 'w') as f:
+        f.write(csv_content)
