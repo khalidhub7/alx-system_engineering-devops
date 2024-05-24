b46d970a4980639f71715b8904d11c69be0856d8
khalidhub7 <louguikhalid7@gmail.com>
2024-05-23 17:26:15 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,7 +2,6 @@
 """ export data in the JSON format. """
 import json
 import requests
-from sys import argv
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/users'
@@ -16,20 +15,19 @@ if __name__ == '__main__':
     for i in range(num_of_employee + 1)[1:]:
         todos = requests.get('{}/{}/todos'.format(url, i)).json()
         all.append(todos)
-
     cc = []
     for j in all:
         for m in j:
+            """ this line is to get username """
             user = next(user for user in data if user['id'] == m['userId'])
+
             cc.append({"task": m['title'],
                        "completed": m['completed'],
-                       "username": user['username']})
-
+                       "username": data['username']})
     content = {}
     for h in range(num_of_employee + 1)[1:]:
         content[str(h)] = [task for task in cc if task['username'] == next(
             user for user in data if user['id'] == h)['username']]
-
     file = 'todo_all_employees.json'
 
     with open(file, 'w') as f:
