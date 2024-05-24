df8c243ecc0628c23ebec9b4467a473de6e291a1
khalidhub7 <louguikhalid7@gmail.com>
2024-05-23 17:20:17 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
-
+""" export data in the JSON format. """
 import json
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    persons = requests.get("\
-https://jsonplaceholder.typicode.com/users/").json()
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users'
+    data = requests.get(url).json()
 
-    with open('todo_all_employees.json', 'w') as file:
-        disctionary = {}
-        for person in persons:
-            todo_list = requests.get("\
-https://jsonplaceholder.typicode.com/users/{}/todos\
-".format(person["id"])).json()
-            if "{}".format(person["id"]) not in disctionary:
-                disctionary["{}".format(person["id"])] = []
-            user = disctionary["{}".format(person["id"])]
-            for i in todo_list:
-                user.append({"username": person["username"],
-                             "task": i["title"],
-                            "completed": i["completed"]})
-        json.dump(disctionary, file)
+    num_of_employee = 0
+    for x in data:
+        num_of_employee += 1
+
+    all = []
+    for i in range(num_of_employee + 1)[1:]:
+        todos = requests.get('{}/{}/todos'.format(url, i)).json()
+        all.append(todos)
+
+    cc = []
+    for j in all:
+        for m in j:
+            user = next(user for user in data if user['id'] == m['userId'])
+            cc.append({"task": m['title'],
+                       "completed": m['completed'],
+                       "username": user['username']})
+
+    content = {}
+    for h in range(num_of_employee + 1)[1:]:
+        content[str(h)] = [task for task in cc if task['username'] == next(
+            user for user in data if user['id'] == h)['username']]
+
+    file = 'todo_all_employees.json'
+
+    with open(file, 'w') as f:
+        json.dump(content, f)
