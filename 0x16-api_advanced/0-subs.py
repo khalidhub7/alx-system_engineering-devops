fd59481de9c20285352d9752c76feb50eae21603
khalidhub7 <louguikhalid7@gmail.com>
2024-06-04 18:41:37 -0700

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
 
+#!/usr/bin/python3
+"""Get the number of subscribers for a subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Get the number of subscribers for a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+
+    try:
+        response = requests.get(url, headers=headers)
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        return data.get('data', {}).get('subscribers', 0)
+    except requests.exceptions.RequestException as e:
         return 0
+
