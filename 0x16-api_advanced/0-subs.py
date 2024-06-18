3e1442e0c1cd20704072a3ca291aec97f77a61b9
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 07:17:41 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,15 @@
 """Gather subscribers from API"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'khalid_loug'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=True)
 
     if response.status_code != 200:
-        return 0
+        return int(0)
     data = response.json().get('data')
     subs = data.get('subscribers')
     return int(subs)
