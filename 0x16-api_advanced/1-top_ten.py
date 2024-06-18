3e19346f278bc4f903ac44c95582f10eb9931f46
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 07:45:18 -0700

    gather title

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,43 @@
+
 #!/usr/bin/python3
-"""Gather subscribers from API"""
+"""
+Contains the top_ten function
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """returns the number of subscribers"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'khalid_loug'}
-    response = requests.get(url, headers=headers, allow_redirects=True)
-    if response.status_code == 200:
-        data = response.json().get('data')
-        subs = data.get('title')
-        print(subs)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+
+    :param subreddit: the name of the subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {
+        "User-Agent": "0x16-api_advanced:project: \
+                v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+
+    try:
+        data = response.json().get("data")
+        children = data.get("children")
+    except ValueError:
+        print(None)
+        return
+
+    if children:
+        for child in children:
+            title = child.get("data").get("title")
+            print(title)
     else:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
