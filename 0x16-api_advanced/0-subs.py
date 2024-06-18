ba1a55c38fab6a41a88c3a7369ed49e32ca6c87d
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 06:16:33 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Gather subscribers from Reddit API.
+Gather subscribers from API
 """
 
 import requests
@@ -8,8 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit=None):
     """
-    Returns the number of subscribers for a given subreddit.
-    If subreddit is invalid or the request fails, returns 0.
+    Returns the number of subscribers
     """
     if subreddit:
         url = f'https://www.reddit.com/r/{subreddit}/about.json'
@@ -20,9 +19,5 @@ def number_of_subscribers(subreddit=None):
             data_returned = response.json()
             return data_returned['data']['subscribers']
         else:
-            print(f"Failed to fetch data for subreddit '{subreddit}': HTTP {response.status_code}")
+            return 0
     return 0
-
-if __name__ == '__main__':
-    subreddit_name = input("Enter the subreddit name: ")
-    print(f"Number of subscribers in r/{subreddit_name}: {number_of_subscribers(subreddit_name)}")
