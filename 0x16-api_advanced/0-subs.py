33b1a0a27d03ba9af8805304141031245c428909
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 06:31:18 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'khalid_loug'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data_returned = response.json()
-        return data_returned['data']['subscribers']
     if response.status_code != 200:
         return 0
+    data = response.json().get('data')
+    subs = data.get('subscribers')
+    return subs
