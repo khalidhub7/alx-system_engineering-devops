37725c163549aff764f2ca9d02c0cbe94026e1c8
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 06:33:19 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'khalid_loug'}
+    headers = {'User-Agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
