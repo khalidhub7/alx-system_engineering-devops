aa3f209a47428366d443b15b49baefe9d84b1e38
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 06:37:08 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""Gather subscribers from API"""
+"""Query Reddit API to determine subreddit sub count
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Request number of subscribers of subreddit
+    from Reddit API
+    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
 
-    if response.status_code != 200:
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return 0
-    data = response.json().get('data')
-    subs = data.get('subscribers')
-    return subs
+
+    # load response unit from json
+    data = r.json()['data']
+    # extract list of pages
+    pages = data['children']
+    # extract data from first page
+    page_data = pages[0]['data']
+    # return number of subreddit subs
+    return page_data['subreddit_subscribers']
