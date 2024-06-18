6fa6f6cfa2a37c263820a784dff5eea272d4e09a
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 05:51:24 -0700

    gather subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" gather subscribers from api """
+import requests
+
+
+def number_of_subscribers(subreddit=None):
+    """ subscribers """
+    if subreddit:
+        resp = requests.get('https://www.reddit.com/r/{}/about.json'
+                            .format(subreddit))
+        if resp.status_code == 200:
+            data_returned = resp.json()
+            return data_returned['data']['subscribers']
+    else:
+        return 0
