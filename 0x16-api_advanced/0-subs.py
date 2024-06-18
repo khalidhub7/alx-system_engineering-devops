5aa4a8e3294233154c897d380f713994c1be30d7
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 06:26:32 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,14 +10,12 @@ def number_of_subscribers(subreddit=None):
     """
     Returns the number of subscribers
     """
-    if subreddit:
-        url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        headers = {'User-Agent': 'khalid_loug'}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data_returned = response.json()
-            return data_returned['data']['subscribers']
-        else:
-            return 0
-    return 0
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'khalid_loug'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data_returned = response.json()
+        return data_returned['data']['subscribers']
+    if response.status_code != 200:
+        return 0
