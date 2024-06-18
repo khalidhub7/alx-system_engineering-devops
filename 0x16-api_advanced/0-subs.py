6c030acc5f4e3993aad56099f8bc4e935122f4e5
khalidhub7 <louguikhalid7@gmail.com>
2024-06-18 05:54:48 -0700

    gather subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" gather subscribers from api """
-import requests
+"""Function to query subscribers on a given Reddit subreddit."""
+from requests import get
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
-def number_of_subscribers(subreddit=None):
-    """ subscribers """
-    if subreddit and isinstance(subreddit, str):
-        resp = requests.get('https://www.reddit.com/r/{}/about.json'
-                            .format(subreddit))
-        if resp.status_code == 200:
-            data_returned = resp.json()
-            return data_returned['data']['subscribers']
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        print("OK")
     else:
-        return 0
+        print("OK")
