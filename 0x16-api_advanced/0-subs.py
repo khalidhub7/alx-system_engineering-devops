312d3b71ba4d1d6fff526acceb20c8a6f75f6d1c
khalidhub7 <louguikhalid7@gmail.com>
2024-06-19 01:48:21 -0700

    gather data #

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-""" gater data from an api """
+"""Gather data from an API"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    responce = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit), headers={
-                                "User-Agent": "khalid_log"
-                            }, allow_redirects=False)
-    if responce.status_code == int(200):
-        data = responce.json()
-        return int(data['data']["subscribers"])
-    elif responce.status_code == int(404):
-        return int(0)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "khalid_log/0.0.1 (by u/your_reddit_username)"
+    }
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return int(data['data']['subscribers'])
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
