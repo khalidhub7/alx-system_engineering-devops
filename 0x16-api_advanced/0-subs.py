7a5959b937113af033945eae4cb0513e76a7f755
khalidhub7 <louguikhalid7@gmail.com>
2024-06-19 01:45:15 -0700

    gather data #

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ def number_of_subscribers(subreddit):
                             .format(subreddit), headers={
                                 "User-Agent": "khalid_log"
                             }, allow_redirects=False)
-    if responce.status_code == 200:
+    if responce.status_code == int(200):
         data = responce.json()
         return int(data['data']["subscribers"])
-    elif responce.status_code != 200:
-        return 0
+    elif responce.status_code == int(404):
+        return int(0)
