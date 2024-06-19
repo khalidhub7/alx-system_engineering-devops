fe0b7b1431f86d7d1d14c6d7343225d4b3b4467e
khalidhub7 <louguikhalid7@gmail.com>
2024-06-19 01:52:58 -0700

    gather data #

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,17 @@
 """Gather data from an API"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given,
+    the function returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "khalid_log/0.0.1 (by u/your_reddit_username)"
     }
-    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
@@ -17,3 +22,10 @@ def number_of_subscribers(subreddit):
             return 0
     except requests.RequestException:
         return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        print(number_of_subscribers(sys.argv[1]))
+    else:
+        print("Usage: {} <subreddit>".format(sys.argv[0]))
