53c3955e82af98f7d16e2324b7ad16ec83bdffb1
khalidhub7 <louguikhalid7@gmail.com>
2024-08-14 16:08:24 -0700

    automate

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-file { 'configure_ssh':
-  path    => '/etc/ssh/ssh_config',
-  ensure  => file,
-  content => "# task 2 in 0x0B-ssh
-IdentityFile ~/.ssh/school
-PasswordAuthentication no
+exec { 'ssh_configuration':
+  command => 'echo "
+# task 2 in 0x0B-ssh
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
 Host ubuntu
     HostName 34.203.38.10
     User ubuntu
-    Port 22",
+    Port 22" >> /etc/ssh/ssh_config',
+  path    => '/bin:/usr/bin',
+  unless  => 'grep -q "Host ubuntu" /etc/ssh/ssh_config',
 }
