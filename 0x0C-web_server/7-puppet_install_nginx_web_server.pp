cd424f48ca9e6dde35c90ca5006a0795839cce3b
khalidhub7 <louguikhalid7@gmail.com>
2024-08-14 19:55:37 -0700

    let puppet do all

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,54 +1,30 @@
-# Install and configure Nginx
-exec { 'update_system':
-  command => 'apt -y update',
-  path    => ['/usr/bin', '/usr/sbin'],
+
+# install nginx
+package { 'nginx':
+  ensure => installed,
 }
 
-exec { 'upgrade_system':
-  command => 'apt -y upgrade',
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['update_system'],
+# website index file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-exec { 'install_nginx':
-  command => 'apt -y install nginx',
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['upgrade_system'],
+# redirect_me config
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => '# this is for task 3 in 0x0C-web_server\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}',
 }
 
-exec { 'create_index_html':
-  command => 'echo "Hello World!" | tee /var/www/html/index.html',
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['install_nginx'],
+# stop nginx
+exec { 'stop service':
+  command => 'sudo service nginx stop',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
 
-exec { 'backup_nginx_config':
-  command => 'cp /etc/nginx/sites-available/default ~/backups/available_default',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => 'test -f ~/backups/available_default',
-  require => Exec['install_nginx'],
-}
-
-exec { 'add_redirection':
-  command => "sed -i '53i\\n\n\n# this is for task 3 in 0x0C-web_server\\n\\tlocation /redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}\\n\\n' /etc/nginx/sites-available/default",
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['backup_nginx_config'],
-}
-
-exec { 'create_custom_404_page':
-  command => "echo \"Ceci n'est pas une page\" | tee /var/www/html/error_404.html",
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['create_index_html'],
-}
-
-exec { 'add_custom_404_page':
-  command => "sed -i '62i\\\terror_page 404 /error_404.html;\\n\\' /etc/nginx/sites-available/default",
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['create_custom_404_page'],
-}
-
-exec { 'restart_nginx':
-  command => 'service nginx restart',
-  path    => ['/usr/bin', '/usr/sbin'],
-  require => Exec['add_custom_404_page'],
+# run nginx
+exec { 'start service':
+  command => 'sudo service nginx start',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
