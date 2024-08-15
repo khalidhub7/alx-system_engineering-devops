a1e7ab0b0c68917797b8f24c7a2753b3d47aa3f9
khalidhub7 <louguikhalid7@gmail.com>
2024-08-14 19:08:30 -0700

    let puppet do all

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,20 @@
-# Setup New Ubuntu server with nginx
-
-# Update system package lists
-exec { 'update system':
-    command => '/usr/bin/apt-get update',
+# Install Nginx
+exec { 'setup_nginx':
+  command => 'apt -y update && apt -y upgrade && apt -y install nginx && echo "Hello World!" | tee /var/www/html/index.html',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
-# Install nginx package
-package { 'nginx':
-    ensure => 'installed',
-    require => Exec['update system'],
+# Backup, Redirection, custom 404 page
+exec { 'configure_nginx':
+  command => 'cp /etc/nginx/sites-available/default ~/backups/available_default && sed -i \'53i\\n\n\n# this is for task 3 in 0x0C-web_server\\n\\tlocation /redirect_me {\\n\\t\\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}\\n\\n\' /etc/nginx/sites-available/default && echo "Ceci n\'est pas une page" | tee /var/www/html/error_404.html && sed -i \'62i\\\terror_page 404 /error_404.html;\\n\\\' /etc/nginx/sites-available/default',
+  path    => ['/usr/bin', '/usr/sbin'],
+  notify  => Exec['restart_nginx'],
+  require => Exec['setup_nginx'],
 }
 
-# Create index.html with "Hello World!" content
-file { '/var/www/html/index.html':
-    content => 'Hello World!',
-}
-
-# Add a rewrite rule to nginx configuration
-exec { 'redirect_me':
-    command => '/bin/sed -i "26i\\       rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-    provider => 'shell',
-    require => Package['nginx'],
-}
-
-# Ensure nginx service is running
-service { 'nginx':
-    ensure => 'running',
-    require => Package['nginx'],
+# restart
+exec { 'restart_nginx':
+  command => 'service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin'],
+  refreshonly => true,
 }
