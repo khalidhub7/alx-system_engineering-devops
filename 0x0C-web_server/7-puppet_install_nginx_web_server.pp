06b3ad33e06b1931215f8f942ec30ef884437a67
khalidhub7 <louguikhalid7@gmail.com>
2024-08-14 19:58:20 -0700

    let puppet do all

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,10 @@ file_line { 'redirect_me':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => '# this is for task 3 in 0x0C-web_server\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}',
+  line   => '
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }',
 }
 
 # stop nginx
@@ -28,3 +31,4 @@ exec { 'start service':
   command => 'sudo service nginx start',
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
+
