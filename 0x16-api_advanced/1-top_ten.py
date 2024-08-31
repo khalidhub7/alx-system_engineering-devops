9ea045f1b79e7ed0b7e5c9caec02cf4e044c5a10
khalidhub7 <louguikhalid7@gmail.com>
2024-08-31 03:50:40 -0700

    up

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""
-Contains the top_ten function
-"""
+"""Fetch the titles"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-
-    :param subreddit: the name of the subreddit
-    """
-    headers = {'User-Agent': 'reddit-top-ten'}
-
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-
+    """10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        'User-Agent': 'MyRedditApp/1.0 (by /u/khalid)'
+    }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-
-            for post in posts:
-                print(post['data']['title'])
-        else:
+        if response.status_code == 302:
             print(None)
-    except Exception:
+            return
+        elif response.status_code == 403:
+            print(None)
+            return
+        elif response.status_code != 200:
+            print(None)
+            return
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+        for i in range(min(10, len(children))):
+            print(children[i].get('data', {}).get('title', ''))
+    except requests.RequestException:
         print(None)
